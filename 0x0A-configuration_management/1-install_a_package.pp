c9ef86c70a0f3646654e2d075e45bc35fd8fa253
NEAZYIT <touihnazih28@gmail.com>
2023-11-24 13:31:18 +0100

    # installs the package puppet-lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Ensures installation of Flask version 2.1.0 using pip3
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin',
-  creates => '/usr/local/lib/python3.8/dist-packages/flask',
+# installs the package puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
