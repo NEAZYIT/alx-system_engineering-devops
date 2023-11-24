761ead91c56402af8ad970d90461b1f166b7b4f0
NEAZYIT <touihnazih28@gmail.com>
2023-11-24 23:17:25 +0100

    Added Puppet manifest for installing Flask version 2.1.0 using pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# This Puppet manifest ensures that Flask version 2.1.0 is installed via pip3
-package { 'flask':
-  ensure   => '2.1.0',   # The version of Flask to be installed
-  provider => 'pip3',     # Specifies that pip3 is used to install Flask
+# Install Flask version 2.1.0 using pip3
+
+exec { 'install_flask': # Defines an 'exec' resource named 'install_flask'
+  command => '/usr/bin/pip3 install Flask==2.1.0', # Specifies the command to install Flask version 2.1.0 using pip3
+  path    => ['/usr/bin', '/usr/local/bin'], # Directories where the command should be found
+  unless  => '/usr/bin/flask --version | grep "Flask 2.1.0"', # Checks if Flask 2.1.0 is already installed
 }
