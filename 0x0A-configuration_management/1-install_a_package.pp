3e9dd0a2a9c839b34c43d45e5b14a276d2a670a7
NEAZYIT <touihnazih28@gmail.com>
2023-11-24 13:41:35 +0100

    Add Puppet manifest to install Flask version 2.1.0 from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# installs the package puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# Ensures installation of python3-pip package
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => '/usr/local/bin:/usr/bin',
+  creates => '/usr/local/lib/python3.11/dist-packages/flask',
 }
