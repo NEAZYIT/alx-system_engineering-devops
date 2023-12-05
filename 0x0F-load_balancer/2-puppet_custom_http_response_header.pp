530a1401149662b4dd14506e576a268f28fbcac1
NEAZYIT <touihnazih28@gmail.com>
2023-12-04 16:39:45 -0800

    This commit sets up a Puppet class named nginx_custom_header to ensure the installation and configuration of Nginx. It utilizes a template named nginx_custom_header.erb to configure Nginx, ensuring the addition of a custom HTTP header named X-Served-By with the value being the hostname of the server handling the requests.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,11 +1,10 @@
-# Puppet class for configuring Nginx with a custom HTTP header
 class nginx_custom_header {
   # Ensure the Nginx package is installed
   package { 'nginx':
     ensure => installed,
   }
 
-  # Configure Nginx
+  # Configure Nginx with the custom HTTP header
   file { '/etc/nginx/sites-available/default':
     ensure  => file,
     # Use the 'nginx_custom_header.erb' template for the configuration
@@ -23,16 +22,4 @@ class nginx_custom_header {
     # The service requires the configuration file to be present
     require => File['/etc/nginx/sites-available/default'],
   }
-}
-# Nginx server configuration
-server {
-    # Listen on port 80
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    # Location block for all requests
-    location / {
-        # Add a custom HTTP header 'X-Served-By' with the value of the server's hostname
-        add_header X-Served-By $hostname;
-    }
 }
