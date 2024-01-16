3bb93d5fb4d49a76267ae401a19343899e1cf2fd
NEAZYIT <touihnazih28@gmail.com>
2024-01-16 07:48:54 -0800

    Add a Python script that leverages the Reddit API to retrieve the total number of subscribers (not active users) for a given subreddit. The script uses the  library to make API requests, setting a custom User-Agent for identification. If the provided subreddit is invalid or if there are issues with the API request, the function gracefully returns 0.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""Function that queries the reddit API"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """Returns the number of subscribers"""
+    url = 'https://www.reddit.com/r/' + subreddit + "/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows)'}
+    res_status = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200 or response.text == "":
+    if res_status.status_code == 200:
+        return res_status.json()['data']['subscribers']
+    else:
         return 0
-
-    results = response.json().get("data")
-    return results.get("subscribers")
