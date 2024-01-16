f2d107df07d89b4b268e71e47329ca82dad40762
NEAZYIT <touihnazih28@gmail.com>
2024-01-16 07:42:52 -0800

    Add a Python script that leverages the Reddit API to retrieve the total number of subscribers (not active users) for a given subreddit. The script uses the  library to make API requests, setting a custom User-Agent for identification. If the provided subreddit is invalid or if there are issues with the API request, the function gracefully returns 0.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,7 @@ Function that queries the Reddit API and returns the number of subscribers
 If an invalid subreddit is given, the function should return 0
 """
 
+import sys
 import requests
 
 
@@ -13,12 +14,31 @@ def number_of_subscribers(subreddit):
     Function that queries the Reddit API
     - If not a valid subreddit, return 0.
     """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                      "AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/91.0.4472.124 Safari/537.36"
+    }
 
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
-    else:
+    try:
+        response = requests.get(
+            f"https://www.reddit.com/r/{subreddit}/about.json",
+            headers=headers
+        )
+
+        if response.status_code == 200:
+            return response.json().get("data").get("subscribers")
+        else:
+            return 0
+
+    except requests.RequestException as e:
+        # Handle exceptions, such as invalid subreddit or network issues
+        print(f"Error: {e}")
         return 0
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
